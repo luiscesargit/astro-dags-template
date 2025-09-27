2844197f15a3302dc6bb06a1c45893563643a923
luiscesargit <103758039+luiscesargit@users.noreply.github.com>
2025-09-27 20:57:28 -0300

    Update OpenFDA.py

--- dags/OpenFDA.py
+++ dags/OpenFDA.py
@@ -8,8 +8,6 @@ from datetime import datetime, timedelta
 import pandas as pd
 import requests
 
-https://api.fda.gov/animalandveterinary/event.json?search=primary_reporter:"Owner"&count=original_receive_date
-
 # ====== CONFIG ======
 GCP_PROJECT  = "linen-walker-470814-e8"    # e.g., "my-gcp-project"
 BQ_DATASET   = "crypto"                    # e.g., "crypto"
@@ -22,7 +20,7 @@ GCP_CONN_ID  = "google_cloud_default"      # Airflow connection with a SA that c
 def generate_query_url(year, month):
     start_date = f"{year}{month:02d}01"
     end_date = f"{year}{month:02d}{(datetime(year, month, 1) + timedelta(days=31)).replace(day=1) - timedelta(days=1):%d}"
-    query = f"https://api.fda.gov/animalandveterinary/event.json?search=primary_reporter:%22Owner%22+AND+count=original_receive_date:[{start_date}+TO+{end_date}]&count=receivedate"
+    query = f"https://api.fda.gov/animalandveterinary/event.json?search=primary_reporter:%22Owner%22+AND+count=original_receive_date:[{start_date}+TO+{end_date}]"
     return query
 
 # Function to fetch data from the API and save it to XCom
@@ -134,6 +132,7 @@ save_data_task = PythonOperator(
 )
 
 fetch_data_task >> save_data_task
+
 
 
 
